0230cfc2a94dc92c0e407bb8498989f4ca6f5d1e
zeez <36535968+zeez2030@users.noreply.github.com>
2019-01-31 14:29:00 +0200

    added footer

--- blog/includes/footer.php
+++ blog/includes/footer.php
@@ -1,5 +1,24 @@
  <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+
+
+    <footer>
+        <div class="social-footer d-flex">
+            <ul class="d-flex">
+                <li><a href="#" ><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+
+            <span class="apec-name">
+                 &reg; 2019 APEC'19 IT Committee.
+            </span>
+        </div>
+
+
+    </footer>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
